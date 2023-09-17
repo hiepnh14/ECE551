0e9ec6f1780802be06a652d1e11c5bd0b55bc143
hhn3 <hhn3@duke.edu>
2023-09-17 15:25:05 -0400

    Submit assignment 28

--- 028_tests_power/test-power.c
+++ 028_tests_power/test-power.c
@@ -4,31 +4,25 @@
 
 unsigned power(unsigned x, unsigned y);
 
-void check(unsigned x, unsigned y, long expected) {
+void check(unsigned x, unsigned y, unsigned expected) {
   if (power(x, y) != expected) {
-    printf("Computed wrongly at %d to the power of %d\n", x, y);
+    printf("Computed wrongly at %d to the power of %d: %d\n", x, y, power(x, y));
     exit(EXIT_FAILURE);
   }
 }
 
 int main(void) {
-  check(0, 1, 0);
   check(0, 0, 1);
   check(1, 0, 1);
   check(2, 0, 1);
-  check(4, 5, 1024);
-  check(0, 10, 0);
-  check(9, 2, 81);
-  check(1, 1, 1);
+  check(0, 2, 0);
   check(1, 2, 1);
-  check(1, 100, 1);
   check(2, 1, 2);
-  check(2, 2, 4);
   check(0, 2, 0);
   check(10, 6, 1000000);
-  check(-1, 0, 0);
+  check(-1, 0, 1);
   check(-1, 1, -1);
-  check(2, 30, 10737411824);
+  //check(2, 30, 10737411824);
   printf("Pass all cases\n");
   return EXIT_SUCCESS;
 }
