21e65e5732afa2efa073b4432d6f662b7b2a87a4
hhn3 <hhn3@duke.edu>
2023-10-02 23:20:27 -0400

    Submit step 2

--- 043_eval1/sunspots.c
+++ 043_eval1/sunspots.c
@@ -20,6 +20,36 @@ ss_monthly_t parseLine(char * line) {
 
 void meanFilter(ss_monthly_t * data, size_t n, ss_monthly_t * mean, unsigned w) {
   // WRITE ME
+  double sum = 0;
+  unsigned long count = 0;
+  unsigned long radius = (w - 1) / 2;
+  for (size_t i = 0; i < n; i++) {
+    sum = 0;
+    count = 0;
+    // Left bound
+    if (i < radius) {
+      for (size_t j = 0; j <= (i + radius); j++) {
+        sum += data[j].num;
+        count++;
+      }
+    }
+    else if ((i + radius) > (n - 1)) {
+      for (size_t j = i - radius; j < n; j++) {
+        sum += data[j].num;
+        count++;
+      }
+    }
+    else {
+      for (size_t j = (i - radius); j <= (i + radius); j++) {
+        sum += data[j].num;
+        count++;
+      }
+    }
+    mean[i].month = data[i].month;
+    mean[i].year = data[i].year;
+    mean[i].num = sum / count;
+    //printf("%lf, %ld\n", mean[i].num, count);
+  }
 }
 
 double findLocalMax(ss_monthly_t * data, size_t n) {
