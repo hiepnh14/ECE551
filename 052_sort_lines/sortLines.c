f3fc757339c63d7d6030da94178ccf68cd5c0781
hhn3 <hhn3@duke.edu>
2023-10-04 06:53:03 -0400

    submit 52

--- 052_sort_lines/sortLines.c
+++ 052_sort_lines/sortLines.c
@@ -14,8 +14,45 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
+void read_sortLine(void * f) {
+  char ** lines = NULL;
+  char * curr = NULL;
+  size_t sz;
+  size_t i = 0;
+  while (getline(&curr, &sz, f) >= 0) {
+    lines = realloc(lines, (i + 1) * sizeof(*lines));
+    lines[i] = curr;
+    curr = NULL;
+    i++;
+  }
+  free(curr);
+  sortData(lines, i);
+  for (size_t j = 0; j < i; j++) {
+    printf("%s", lines[j]);
+    free(lines[j]);
+  }
+  free(lines);
+}
+
 int main(int argc, char ** argv) {
   //WRITE YOUR CODE HERE!
-
+  if (argc == 1) {
+    //printf("Enter the string to be ordered");
+    read_sortLine(stdin);
+  }
+  else if (argc > 1) {
+    for (size_t i = 1; i < argc; i++) {
+      FILE * f = fopen(argv[i], "r");
+      if (f == NULL) {
+        printf("File %s not found", argv[i]);
+        return EXIT_FAILURE;
+      }
+      read_sortLine(f);
+      if (fclose(f) != 0) {
+        fprintf(stderr, "Fail to close the file");
+        return EXIT_FAILURE;
+      }
+    }
+  }
   return EXIT_SUCCESS;
 }
