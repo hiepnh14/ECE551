dfdfb7714fa0c20906ff366dccb7666ec8e97eb6
hhn3 <hhn3@duke.edu>
2023-10-11 00:31:58 -0400

    Submit

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -1,8 +1,15 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  char * suffix = ".counts";
+  size_t len = strlen(inputName) + strlen(suffix) + 1;
+  char * output = malloc(len * sizeof(output));
+  strcpy(output, inputName);
+  strcat(output, suffix);
+  return output;
 }
