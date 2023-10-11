7111a1af22ce204a5f663abcc24ec3e4b8ff53fe
hhn3 <hhn3@duke.edu>
2023-10-11 15:04:09 -0400

    submit

--- 058_counts/counts.c
+++ 058_counts/counts.c
@@ -1,17 +1,61 @@
+#include "counts.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "counts.h"
 counts_t * createCounts(void) {
   //WRITE ME
+  counts_t * c = malloc(sizeof(*c));
+  c->counts = NULL;
+  c->numCount = 0;
+  c->unknownCount = 0;
+  return c;
 }
 void addCount(counts_t * c, const char * name) {
   //WRITE ME
+  if (name == NULL) {
+    c->unknownCount++;
+    return;
+  }
+  int len = c->numCount;
+  for (int i = 0; i < len; i++) {
+    if (strcmp(c->counts[i]->key, name) == 0) {
+      c->counts[i]->count++;
+      return;
+    }
+  }
+  c->counts = realloc(c->counts, (len + 1) * sizeof(*c->counts));
+  c->counts[len] = malloc(sizeof(*c->counts[len]));
+  c->counts[len]->key = name;
+  c->counts[len]->count++;
+  c->numCount++;
 }
 void printCounts(counts_t * c, FILE * outFile) {
   //WRITE ME
+  if (c == NULL) {
+    fprintf(stderr, "No data in counts");
+    exit(EXIT_FAILURE);
+  }
+  if (outFile == NULL) {
+    fprintf(stderr, "Cannot open outFile");
+    exit(EXIT_FAILURE);
+  }
+  int len = c->numCount;
+  for (int i = 0; i < len; i++) {
+    fprintf(outFile, "%s: %ld\n", c->counts[i]->key, c->counts[i]->count);
+  }
+  if (c->unknownCount != 0) {
+    fprintf(outFile, "<unknown> : %ld\n", c->unknownCount);
+  }
 }
-
 void freeCounts(counts_t * c) {
   //WRITE ME
+  int len = c->numCount;
+  for (int i = 0; i < len; i++) {
+    //free(c->counts[i]->key);
+    //free(c->counts[i]->count);
+    free(c->counts[i]);
+  }
+  free(c->counts);
+  free(c);
 }
--- 058_counts/counts.h
+++ 058_counts/counts.h
@@ -1,13 +1,18 @@
 #ifndef __COUNTS_H__
+#include <stdio.h>
 #define __COUNTS_H__
 struct _one_count_t {
   //DEFINE ME
-
+  const char * key;
+  size_t count;
 };
 typedef struct _one_count_t one_count_t;
 
 struct _counts_t {
   //DEFINE ME
+  one_count_t ** counts;
+  size_t numCount;
+  size_t unknownCount;
 };
 typedef struct _counts_t counts_t;
 
