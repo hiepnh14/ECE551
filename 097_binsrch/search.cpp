cfbcfc0c9b2e07e836b1b11addbf868c69a371bb
hhn3 <hhn3@duke.edu>
2023-12-01 02:03:11 -0500

    Checking

--- 097_binsrch/search.cpp
+++ 097_binsrch/search.cpp
@@ -15,19 +15,18 @@ int binarySearchForZero(Function<int, int> * f, int low, int high) {
   int x = (high + low) / 2;
   int y;
   while (low <= high) {
+    x = (high + low) / 2;
     y = f->invoke(x);
     if (y == 0)
       return x;
     else if (y < 0) {
       low = x + 1;
-      x = (high + low) / 2;
     }
     else {
       high = x - 1;
-      x = (high + low) / 2;
     }
   }
   if (y > 0)
-    return x - 1;
+    return low;
   return x;
 }
