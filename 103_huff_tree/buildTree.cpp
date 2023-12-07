191a1596844913b2c6b3ce6542634de3697876e2
hhn3 <hhn3@duke.edu>
2023-12-06 21:58:47 -0500

    Submit

--- 103_huff_tree/buildTree.cpp
+++ 103_huff_tree/buildTree.cpp
@@ -1,5 +1,21 @@
 #include "node.h"
 
-Node *  buildTree(uint64_t * counts) {
+Node * buildTree(uint64_t * counts) {
   //WRITE ME!
-}  
+  priority_queue_t priority_queue;
+  for (size_t i = 0; i < 257; i++) {
+    if (counts[i] != 0)
+      priority_queue.push(new Node(i, counts[i]));
+  }
+  while (priority_queue.size() != 1) {
+    Node * firstNode = priority_queue.top();
+    priority_queue.pop();
+    Node * secondNode = priority_queue.top();
+    priority_queue.pop();
+    Node * combineNodes = new Node(firstNode, secondNode);
+    priority_queue.push(combineNodes);
+  }
+  Node * root = priority_queue.top();
+  priority_queue.pop();
+  return root;
+}
